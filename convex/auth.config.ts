ec8d9eb428285d8e386212c110d8c3a2889fb8d8
EnO33 <mattmar33@gmail.com>
2024-06-05 16:12:02 +0200

    convex with clerk

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+  providers: [
+    {
+      domain: "https://enough-mollusk-76.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
+
+export default authConfig;
