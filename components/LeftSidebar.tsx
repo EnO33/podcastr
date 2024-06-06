d984ee92797bd04e35af6529e4befde4bc3100ac
EnO33 <mattmar33@gmail.com>
2024-06-06 12:35:24 +0200

    right sidebar

--- components/LeftSidebar.tsx
+++ components/LeftSidebar.tsx
@@ -2,13 +2,16 @@
 
 import { sidebarLinks } from "@/constants";
 import { cn } from "@/lib/utils";
+import { SignedIn, SignedOut, useClerk } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname, useRouter } from "next/navigation";
+import { Button } from "./ui/button";
 
 const LeftSidebar = () => {
   const pathname = usePathname();
   const router = useRouter();
+  const { signOut } = useClerk();
   return (
     <section className="left_sidebar">
       <nav className="flex flex-col gap-6">
@@ -32,7 +35,7 @@ const LeftSidebar = () => {
               className={cn(
                 "flex gap-3 items-center py-4 max-lg:px-4 justify-center lg:justify-start",
                 {
-                    'bg-nav-focus border-r-4 border-orange-1': isActive
+                  "bg-nav-focus border-r-4 border-orange-1": isActive,
                 }
               )}
             >
@@ -42,6 +45,23 @@ const LeftSidebar = () => {
           );
         })}
       </nav>
+      <SignedOut>
+        <div className="flex-center w-full pb-14 max-lg:px-4 lg:pr-8">
+          <Button className="text-16 w-full bg-orange-1 font-extrabold" asChild>
+            <Link href="/sign-in">Sign in</Link>
+          </Button>
+        </div>
+      </SignedOut>
+      <SignedIn>
+        <div className="flex-center w-full pb-14 max-lg:px-4 lg:pr-8">
+          <Button
+            className="text-16 w-full bg-orange-1 font-extrabold"
+            onClick={() => signOut(() => router.push("/"))}
+          >
+            Log out
+          </Button>
+        </div>
+      </SignedIn>
     </section>
   );
 };
