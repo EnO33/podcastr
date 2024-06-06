c0710ecc95dfa205e287517cd9bde32472707285
EnO33 <mattmar33@gmail.com>
2024-06-06 13:47:16 +0200

    podcastPlayer

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -3,6 +3,7 @@ import MobileNav from "@/components/MobileNav";
 import RightSidebar from "@/components/RightSidebar";
 import Image from "next/image";
 import { Toaster } from "@/components/ui/toaster";
+import PodcastPlayer from "@/components/PodcastPlayer";
 
 export default function RootLayout({
   children,
@@ -17,7 +18,12 @@ export default function RootLayout({
         <section className="flex min-h-screen flex-1 flex-col px-4 sm:px-14">
           <div className="mx-auto flex w-full max-w-5xl flex-col max-sm:px-4">
             <div className="flex h-16 items-center justify-between md:hidden">
-              <Image src="/icons/logo.svg" width={30} height={30} alt="Menu icon" />
+              <Image
+                src="/icons/logo.svg"
+                width={30}
+                height={30}
+                alt="Menu icon"
+              />
               <MobileNav />
             </div>
             <div className="flex flex-col md:pb-14">
@@ -29,6 +35,7 @@ export default function RootLayout({
 
         <RightSidebar />
       </main>
+      <PodcastPlayer />
     </div>
   );
 }
